a0e198e1102be12e0a0f1bcd1fbae53c4ca27974
Brayden Chaffee <chaffee.brayden@gmail.com>
2025-08-06 20:17:58 -0400

    Nvim tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,12 @@
+-- lua/plugins/nvim-tree.lua
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup {}
+      vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = "Toggle File Tree" })
+    end,
+  }
+}
+
