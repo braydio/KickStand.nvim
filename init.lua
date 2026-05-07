358e9f336098a4bd0290b3732b132ad1618bd034
Brayden Chaffee <chaffee.brayden@gmail.com>
2026-05-07 14:40:26 -0400

    Add planner autocmd

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ vim.g.maplocalleader = ' '
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Load configuration modules
+-- Load configuration
 require('config')
 
 -- Bootstrap lazy.nvim
@@ -27,3 +27,41 @@ require('lazy').setup({
   install = { colorscheme = { 'catppuccin', 'tokyonight', 'rose-pine', 'nightfox', 'gruvbox', 'onedark' } },
   git = { url_format = 'git@github.com:%s.git' },
 })
+
+-- Planner Command (Move to proper directory asap)
+vim.cmd [[
+  function! SetDailyTemplate()
+    let l:date = strftime("%A, %B %d, %Y")
+    execute "normal! ggdG"
+    call append(0, "# Daily Planner - " . l:date)
+    call append(1, "")
+    call append(2, "## Morning Goals & Priorities")
+    call append(3, "- [ ] Example: Review emails")
+    call append(4, "- [ ] Example: Set top 3 priorities")
+    call append(5, "")
+    call append(6, "## Schedule")
+    call append(7, "08:00 - 09:00 :")
+    call append(8, "09:00 - 10:00 :")
+    call append(9, "10:00 - 11:00 :")
+    call append(10, "…")
+    call append(11, "")
+    call append(12, "## Tasks")
+    call append(13, "- [ ] Task 1")
+    call append(14, "- [ ] Task 2")
+    call append(15, "")
+    call append(16, "## Evening Reflection & Notes")
+    call append(17, "- What went well?")
+    call append(18, "- What could be improved?")
+    call append(19, "- Notes:")
+    call append(20, "- One notable thing about today is...")
+    call append(21, "- Reflections:")
+    call append(22, "- One thing to reflect one about today is...")
+  normal! G
+  endfunction
+
+  augroup DailyPlanner
+    autocmd!
+    autocmd BufNewFile ~/Documents/Journal/Planner/*.md call SetDailyTemplate()
+  augroup END
+]]
+
